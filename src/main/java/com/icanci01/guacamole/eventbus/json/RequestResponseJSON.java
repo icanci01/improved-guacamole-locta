8be25bc4db96b7d4c82b670beb18863c77452898
Cânciu Ionuț - Cristian <icanci01@ucy.ac.cy>
2021-08-13 15:53:24 +0300

    Fixed small issues

--- src/main/java/com/icanci01/guacamole/eventbus/json/RequestResponseJSON.java
+++ src/main/java/com/icanci01/guacamole/eventbus/json/RequestResponseJSON.java
@@ -1,6 +1,7 @@
 package com.icanci01.guacamole.eventbus.json;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
@@ -13,49 +14,63 @@ public class RequestResponseJSON extends AbstractVerticle {
     @Override
     public void start(final Promise<Void> startPromise) throws Exception {
         LOG.debug("Starting {}", getClass().getName());
-        vertx.deployVerticle(new RequestVerticleJSON());
-        vertx.deployVerticle(new ResponseVerticleJSON());
+        vertx.deployVerticle(new RequestVerticleJSONObject(), new DeploymentOptions(),
+            whenDeployed -> vertx.undeploy(whenDeployed.result()));
+        vertx.deployVerticle(new ResponseVerticleJSONObject(), new DeploymentOptions(),
+            whenDeployed -> vertx.undeploy(whenDeployed.result()));
         startPromise.complete();
     }
 
-    static class RequestVerticleJSON extends AbstractVerticle {
+    static class RequestVerticleJSONObject extends AbstractVerticle {
 
-        private static final Logger LOG = LoggerFactory.getLogger(RequestVerticleJSON.class);
+        private static final Logger LOG = LoggerFactory.getLogger(RequestVerticleJSONObject.class);
         private static final String ADDRESS = "my.request.address.json";
 
         @Override
         public void start(final Promise<Void> startPromise) throws Exception {
             LOG.debug("Start {}", getClass().getSimpleName());
-            startPromise.complete();
             var eventBus = vertx.eventBus();
             var message = new JsonObject()
                 .put("message", "Hello from event bus!")
-                .put("id", 4);
+                .put("id", 4)
+                .put("version", 1);
             LOG.debug(">>> Sending:  \"{}\"", message.toString());
             eventBus.<JsonObject>request(ADDRESS, message, reply -> {
                 LOG.debug("<<< Response: \"{}\"", reply.result().body().toString());
             });
+            startPromise.complete();
+        }
 
+        @Override
+        public void stop(final Promise<Void> stopPromise) throws Exception {
+            LOG.debug("\tStop {}", getClass().getSimpleName());
+            stopPromise.complete();
         }
 
     }
 
-    static class ResponseVerticleJSON extends AbstractVerticle {
+    static class ResponseVerticleJSONObject extends AbstractVerticle {
 
-        private static final Logger LOG = LoggerFactory.getLogger(ResponseVerticleJSON.class);
+        private static final Logger LOG = LoggerFactory.getLogger(ResponseVerticleJSONObject.class);
 
         @Override
         public void start(final Promise<Void> startPromise) throws Exception {
             LOG.debug("\tStart {}", getClass().getSimpleName());
-            startPromise.complete();
-            vertx.eventBus().<JsonObject>consumer(RequestVerticleJSON.ADDRESS, message -> {
+            vertx.eventBus().<JsonObject>consumer(RequestVerticleJSONObject.ADDRESS, message -> {
                 LOG.debug("\t>> [Received Message -- \"{}\"]", message.body().toString());
                 var responseMessage = new JsonObject()
                     .put("message", "Received your message! Thanks!")
                     .put("id", 5);
                 LOG.debug("\t<< [Sending Response -- \"{}\"]", responseMessage);
                 message.reply(responseMessage);
+                startPromise.complete();
             });
+        }
+
+        @Override
+        public void stop(final Promise<Void> stopPromise) throws Exception {
+            LOG.debug("\tStop {}", getClass().getSimpleName());
+            stopPromise.complete();
         }
 
     }
