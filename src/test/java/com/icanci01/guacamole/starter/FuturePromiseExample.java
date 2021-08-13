e1578e5ddb8e097b91b8656005f1b9d84e4b823b
Cânciu Ionuț - Cristian <icanci01@ucy.ac.cy>
2021-08-13 21:25:52 +0300

    Futures and Promises changes

--- src/test/java/com/icanci01/guacamole/starter/FuturePromiseExample.java
+++ src/test/java/com/icanci01/guacamole/starter/FuturePromiseExample.java
@@ -5,24 +5,121 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+@ExtendWith(VertxExtension.class)
 public class FuturePromiseExample {
 
     private static final Logger LOG = LoggerFactory.getLogger(FuturePromiseExample.class);
 
-    public static void main(String[] args) {
-        LOG.debug("Starting {}", FuturePromiseExample.class.getSimpleName());
-        var vertx = Vertx.vertx();
+    /**
+     * Asynchronous call with a delay of 500 ms
+     * Call successes example
+     *
+     * @param vertx   - var vertx = Vertx.vertx();
+     * @param context - VertxTestContext
+     */
+    @Test
+    void promise_success(Vertx vertx, VertxTestContext context) {
+        LOG.debug("Start test");
+
         final Promise<String> promise = Promise.promise();
-        vertx.setPeriodic(500, id ->
-            promise.complete("Success!"));
+
+        vertx.setPeriodic(500, id -> {
+            promise.complete("Success");
+            LOG.debug("Success");
+            context.completeNow();
+        });
+
+        LOG.debug("End test");
+    }
+
+    /**
+     * Asynchronous call with a delay of 500 ms
+     * Call fails example
+     *
+     * @param vertx   - var vertx = Vertx.vertx();
+     * @param context - VertxTestContext
+     */
+    @Test
+    void promise_fail(Vertx vertx, VertxTestContext context) {
+        LOG.debug("Start test");
+
+        final Promise<String> promise = Promise.promise();
+
+        vertx.setPeriodic(500, id -> {
+            promise.fail(new RuntimeException("Failed!"));
+            LOG.debug("Failed");
+            context.completeNow();
+        });
+
+        LOG.debug("End test");
+    }
+
+    /**
+     * Asynchronous call with a delay of 500 ms
+     * Call successes example
+     *
+     * @param vertx   - var vertx = Vertx.vertx();
+     * @param context - VertxTestContext
+     */
+    @Test
+    void future_success(Vertx vertx, VertxTestContext context) {
+        LOG.debug("Start test");
+
+        final Promise<String> promise = Promise.promise();
+
+        vertx.setPeriodic(500, id -> {
+            promise.complete("Success");
+            LOG.debug("Timer Done");
+        });
+
         final Future<String> future = promise.future();
         future
-            .onSuccess(Future::succeededFuture)
-            .onFailure(Future::failedFuture);
+            .onSuccess(result -> {
+                LOG.debug("End Future Result: {}", result);
+                context.completeNow();
+            })
+            .onFailure(context::failNow);
+
+        LOG.debug("End test");
+    }
+
+    /**
+     * Asynchronous call with a delay of 500 ms
+     * Call fails example
+     *
+     * @param vertx   - var vertx = Vertx.vertx();
+     * @param context - VertxTestContext
+     */
+    @Test
+    void future_fail(Vertx vertx, VertxTestContext context) {
+        LOG.debug("Start test");
+
+        final Promise<String> promise = Promise.promise();
+
+        vertx.setPeriodic(500, id -> {
+            promise.fail(new RuntimeException("Failed!"));
+            LOG.debug("Timer done");
+        });
+
+        final Future<String> future = promise.future();
+        future
+            .onSuccess(context::failNow)
+            .onFailure(error -> {
+                LOG.error("Error:", error);
+                context.completeNow();
+            });
+
+        LOG.debug("End test");
+    }
+
+    public static void main(String[] args) {
 
         final Promise<Void> returnVoid = Promise.promise();
         returnVoid.complete();
@@ -34,6 +131,7 @@ public class FuturePromiseExample {
         final Promise<Void> one = Promise.promise();
         final Promise<Void> two = Promise.promise();
         final Promise<Void> three = Promise.promise();
+
         final Future<Void> futureOne = one.future();
         final Future<Void> futureTwo = two.future();
         final Future<Void> futureThree = three.future();
